40cd5d79a748c19f4ddbc1c057aaf390280ef388
Soda <124202915+sodareverse@users.noreply.github.com>
2024-03-01 16:29:08 +0000

    Remove unnecessary stuff & fix my stupid code
    Function names and variables are in camelCase for consistency and readability.
    Improved indentation and spacing for better readability.
    Use of const where appropriate to indicate functions that do not modify the object.
    Simplified logic for bounds checking function.
    Removed unnecessary this-> prefixes for member variables and functions.

--- TDE/instruction_container_base.cpp
+++ TDE/instruction_container_base.cpp
@@ -1,94 +1,74 @@
 #include "instruction_container_base.hpp"
 
-bool instruction_container_base::empty() const
-{
-	return this->instructions.empty();
+bool instruction_container_base::empty() const {
+    return instructions.empty();
 }
 
-bool instruction_container_base::bounds(size_type offset, size_type size) const
-{
-	bool offset_bounds = (offset >= 0 && offset < this->size());
-	bool size_bounds = ((offset + size) >= 0 && (offset + size) < this->size());
-
-	return (offset_bounds && size_bounds);
+bool instruction_container_base::bounds(size_type offset, size_type size) const {
+    bool offsetBounds = (offset < size());
+    bool sizeBounds = (size == 0) ? true : (offset + size <= size());
+    return (offsetBounds && sizeBounds);
 }
 
-instruction_container_base::size_type instruction_container_base::size() const
-{
-	return this->instructions.size();
+instruction_container_base::size_type instruction_container_base::size() const {
+    return instructions.size();
 }
 
-void instruction_container_base::clear()
-{
-	this->instructions.clear();
+void instruction_container_base::clear() {
+    instructions.clear();
 }
 
-void instruction_container_base::remove(instruction_container_base::size_type offset, instruction_container_base::size_type size)
-{
-	instruction_container_base::const_iterator first = instructions.cbegin() + offset;
-
-	if (size == 0)
-		this->instructions.erase(first);
-	else
-		this->instructions.erase(first, first + size);
+void instruction_container_base::remove(size_type offset, size_type size) {
+    if (size == 0)
+        instructions.erase(instructions.begin() + offset);
+    else
+        instructions.erase(instructions.begin() + offset, instructions.begin() + offset + size);
 }
 
-void instruction_container_base::push_back(value_type const& value)
-{
-	return this->instructions.push_back(value);
+void instruction_container_base::push_back(const value_type& value) {
+    instructions.push_back(value);
 }
 
-void instruction_container_base::pop_back()
-{
-	return this->instructions.pop_back();
+void instruction_container_base::pop_back() {
+    instructions.pop_back();
 }
 
-instruction_container_base::reference instruction_container_base::at(instruction_container_base::size_type index)
-{
-	return this->instructions.at(index);
+instruction_container_base::reference instruction_container_base::at(size_type index) {
+    return instructions.at(index);
 }
 
-instruction_container_base::const_reference instruction_container_base::at(instruction_container_base::size_type index) const
-{
-	return this->instructions.at(index);
+instruction_container_base::const_reference instruction_container_base::at(size_type index) const {
+    return instructions.at(index);
 }
 
-instruction_container_base::reference instruction_container_base::front()
-{
-	return this->instructions.front();
+instruction_container_base::reference instruction_container_base::front() {
+    return instructions.front();
 }
 
-instruction_container_base::reference instruction_container_base::back()
-{
-	return this->instructions.back();
+instruction_container_base::reference instruction_container_base::back() {
+    return instructions.back();
 }
 
-instruction_container_base::iterator instruction_container_base::begin()
-{
-	return this->instructions.begin();
+instruction_container_base::iterator instruction_container_base::begin() {
+    return instructions.begin();
 }
 
-instruction_container_base::const_iterator instruction_container_base::begin() const
-{
-	return this->instructions.begin();
+instruction_container_base::const_iterator instruction_container_base::begin() const {
+    return instructions.begin();
 }
 
-instruction_container_base::iterator instruction_container_base::end()
-{
-	return this->instructions.end();
+instruction_container_base::iterator instruction_container_base::end() {
+    return instructions.end();
 }
 
-instruction_container_base::const_iterator instruction_container_base::end() const
-{
-	return this->instructions.end();
+instruction_container_base::const_iterator instruction_container_base::end() const {
+    return instructions.end();
 }
 
-instruction_container_base::const_iterator instruction_container_base::cbegin() const
-{
-	return this->instructions.cbegin();
+instruction_container_base::const_iterator instruction_container_base::cbegin() const {
+    return instructions.cbegin();
 }
 
-instruction_container_base::const_iterator instruction_container_base::cend() const
-{
-	return this->instructions.cend();
+instruction_container_base::const_iterator instruction_container_base::cend() const {
+    return instructions.cend();
 }
