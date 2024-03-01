d5a55964676f09cec69496d589f63dc01a93d1aa
de-virtualized <161866845+de-virtualized@users.noreply.github.com>
2024-03-01 17:54:14 +0100

    Removed unnecessary parentheses in the return statement of size() method.
    Done other optional improvements to make the code faster and better

--- TDE/segment_copy.cpp
+++ TDE/segment_copy.cpp
@@ -2,9 +2,10 @@
 #include <idp.hpp>
 #include <segment.hpp>
 
-SegmentCopy::SegmentCopy(uint32_t startAddress, uint32_t endAddress) : segmentInfo(std::make_pair(startAddress, endAddress)) {
+SegmentCopy::SegmentCopy(uint32_t startAddress, uint32_t endAddress)
+    : segmentInfo(std::make_pair(startAddress, endAddress)) {
     segmentData.resize(endAddress - startAddress);
-    get_many_bytes(startAddress, &segmentData[0], endAddress - startAddress);
+    get_many_bytes(startAddress, &segmentData[0], segmentData.size());
 }
 
 uint32_t SegmentCopy::start() const {
@@ -16,9 +17,9 @@ uint32_t SegmentCopy::end() const {
 }
 
 uint32_t SegmentCopy::size() const {
-    return (end() - start());
+    return end() - start();
 }
 
 uint8_t* SegmentCopy::data(uint32_t offset) {
-    return (segmentData.data() + offset);
+    return segmentData.data() + offset;
 }
