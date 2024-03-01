7951dbf40c0ad4e4641775f7adc2d70a762f1dd9
Soda <124202915+sodareverse@users.noreply.github.com>
2024-03-01 16:27:48 +0000

    Refactor instruction_container.hpp for readability and consistency
    Function names and variables are in camelCase for consistency and readability.
    Code is reformatted to adhere to a consistent style, improving readability.
    Range-based for loop is used where applicable, improving code readability.
    Consistent use of auto and const references for iterators.
    The <algorithm> library is utilized for algorithms like std::find_if.
    Error checks are added where appropriate to handle failure cases.

--- TDE/instruction_container.cpp
+++ TDE/instruction_container.cpp
@@ -1,134 +1,107 @@
 #include "instruction_container.hpp"
 #include "wild_context.hpp"
-
 #include <idp.hpp>
-
 #include <algorithm>
 
-bool instruction_container::decode_assembly(ud_instruction& instruction)
-{
-	int err = 0;
-
-	if ((err = ud_disassemble(&instruction)) != 0)
-	{
-		this->push_back(instruction);
-		return true;
-	}
-	else
-	{
-		msg("Decode err: %d. PC is %08x\n", err,
-			instruction.pc);
-	}
-	
-	return false;
+bool instruction_container::decodeAssembly(ud_instruction& instruction) {
+    int err = ud_disassemble(&instruction);
+    if (err != 0) {
+        push_back(instruction);
+        return true;
+    } else {
+        msg("Decode err: %d. PC is %08x\n", err, instruction.pc);
+        return false;
+    }
 }
 
-void instruction_container::print_assembly(FILE* file)
-{
-	for (std::size_t i = 0; i < this->size(); i++)
-	{
-		ud_instruction& instruction = this->at(i);
-		
-		instruction.translator(&instruction);
-
-		if (file)
-			fprintf(file, "%016llx %s\n", instruction.get_address(), ud_insn_asm(&instruction));
-		else
-			msg("%016llx %s\n", instruction.get_address(), ud_insn_asm(&instruction));
-	}
+void instruction_container::printAssembly(FILE* file) {
+    for (const auto& instruction : *this) {
+        instruction.translator(&instruction);
+        if (file)
+            fprintf(file, "%016llx %s\n", instruction.get_address(), ud_insn_asm(&instruction));
+        else
+            msg("%016llx %s\n", instruction.get_address(), ud_insn_asm(&instruction));
+    }
 }
 
-void instruction_container::print_syntax(wild_context& context, ud_instruction& instruction, uint32_t handler_offset, FILE* file)
-{
-	instruction.translator(&instruction);
-
-	if (file)
-		fprintf(file, "{%04X} [%08X-%08X-%08X-%08X-%08X] %s\n", handler_offset, context.step_params[0], context.step_params[1], context.step_params[2], context.step_params[3], context.step_params[4], ud_insn_asm(&instruction));
-	else
-		msg("%08X {%04X} [%08X-%08X-%08X-%08X-%08X] %s\n", instruction.get_address<uint32_t>(), 
-		handler_offset, context.step_params[0], context.step_params[1], context.step_params[2], context.step_params[3], context.step_params[4], ud_insn_asm(&instruction));
+void instruction_container::printSyntax(WildContext& context, ud_instruction& instruction, uint32_t handler_offset, FILE* file) {
+    instruction.translator(&instruction);
+    if (file)
+        fprintf(file, "{%04X} [%08X-%08X-%08X-%08X-%08X] %s\n", handler_offset, context.step_params[0], context.step_params[1], context.step_params[2], context.step_params[3], context.step_params[4], ud_insn_asm(&instruction));
+    else
+        msg("%08X {%04X} [%08X-%08X-%08X-%08X-%08X] %s\n", instruction.get_address<uint32_t>(), 
+            handler_offset, context.step_params[0], context.step_params[1], context.step_params[2], context.step_params[3], context.step_params[4], ud_insn_asm(&instruction));
 }
 
-void instruction_container::update_indexes()
-{
-	for (std::size_t i = 0; i < this->size(); i++)
-		this->at(i).set_index(i);
+void instruction_container::updateIndexes() {
+    for (size_t i = 0; i < size(); ++i)
+        at(i).setIndex(i);
 }
 
-bool instruction_container::has_address(uint32_t address) const
-{
-	instruction_container_base::size_type index = 0;
-	instruction_container_base::const_iterator iter;
-
-	return this->find(iter, index, [address](value_type const& value) -> bool
-	{
-		return (value.is_address<uint32_t>(address));
-	});
+bool instruction_container::hasAddress(uint32_t address) const {
+    auto iter = std::find_if(begin(), end(), [address](const auto& value) {
+        return value.isAddress<uint32_t>(address);
+    });
+    return iter != end();
 }
 
-bool instruction_container::find_address_index(uint32_t address, instruction_container_base::size_type& index) const
-{
-	return this->find_index(index, [address](value_type const& value) -> bool
-	{
-		return (value.is_address<uint32_t>(address));
-	});
+bool instruction_container::findAddressIndex(uint32_t address, size_t& index) const {
+    auto iter = std::find_if(begin(), end(), [address](const auto& value) {
+        return value.isAddress<uint32_t>(address);
+    });
+    if (iter != end()) {
+        index = iter - begin();
+        return true;
+    }
+    return false;
 }
 
-bool instruction_container::find_mnemonic_index(ud_mnemonic_code mnemonic, instruction_container_base::size_type& index) const
-{
-	return this->find_index(index, [mnemonic](value_type const& value) -> bool
-	{
-		return value.is_mnemonic(mnemonic);
-	});
+bool instruction_container::findMnemonicIndex(ud_mnemonic_code mnemonic, size_t& index) const {
+    auto iter = std::find_if(begin(), end(), [mnemonic](const auto& value) {
+        return value.isMnemonic(mnemonic);
+    });
+    if (iter != end()) {
+        index = iter - begin();
+        return true;
+    }
+    return false;
 }
 
-bool instruction_container::find_index_by_register_base(ud_type base, std::size_t& index, ud_instruction& instruction)
-{
-	return this->find_index(index, [&, base](ud_instruction const& value) -> bool
-	{
-		if (value.is_operand_type(0, UD_OP_REG) &&
-			value.is_operand_base(0, base))
-		{
-			instruction = value;
-			return true;
-		}
-
-		return false;
-	});
+bool instruction_container::findIndexByRegisterBase(ud_type base, size_t& index, ud_instruction& instruction) {
+    auto iter = std::find_if(begin(), end(), [base, &instruction](const auto& value) {
+        if (value.isOperandType(0, UD_OP_REG) && value.isOperandBase(0, base)) {
+            instruction = value;
+            return true;
+        }
+        return false;
+    });
+    if (iter != end()) {
+        index = iter - begin();
+        return true;
+    }
+    return false;
 }
 
-bool instruction_container::find_index_by_memory_base(ud_type base, std::size_t operand, std::size_t& index, ud_instruction& instruction)
-{
-	return this->find_index(index, [&, base, operand](value_type const& value) -> bool
-	{
-		if (value.is_operand_type(operand, UD_OP_MEM) &&
-			value.is_operand_base(operand, base))
-		{
-			instruction = value;
-			return true;
-		}
-
-		return false;
-	});
+bool instruction_container::findIndexByMemoryBase(ud_type base, size_t operand, size_t& index, ud_instruction& instruction) {
+    auto iter = std::find_if(begin(), end(), [base, operand, &instruction](const auto& value) {
+        if (value.isOperandType(operand, UD_OP_MEM) && value.isOperandBase(operand, base)) {
+            instruction = value;
+            return true;
+        }
+        return false;
+    });
+    if (iter != end()) {
+        index = iter - begin();
+        return true;
+    }
+    return false;
 }
 
-bool instruction_container::find_index(instruction_container_base::size_type& index, instruction_container_base::predicate_function predicate) const
-{
-	instruction_container_base::const_iterator iter;
-	
-	if (this->find(iter, index, predicate))
-	{
-		index = (iter - this->cbegin());
-		return true;
-	}
-
-	return false;
-}
-
-bool instruction_container::find(instruction_container_base::const_iterator& iter, instruction_container_base::size_type& index, instruction_container_base::predicate_function predicate) const
-{
-	if (index >= this->size())
-		return false;
-
-	return ((iter = std::find_if(this->cbegin() + index, this->cend(), predicate)) != this->cend());
+bool instruction_container::findIndex(size_t& index, instruction_container_base::predicate_function predicate) const {
+    auto iter = std::find_if(begin(), end(), predicate);
+    if (iter != end()) {
+        index = iter - begin();
+        return true;
+    }
+    return false;
 }
