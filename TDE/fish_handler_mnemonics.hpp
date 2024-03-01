c8ad6113c1213c6020994e5f37190b7b9e8c0b4b
Soda <124202915+sodareverse@users.noreply.github.com>
2024-03-01 16:12:38 +0000

    Create fish_handler_mnemonics.hpp

---
+++ TDE/fish_handler_mnemonics.hpp
@@ -0,0 +1,13 @@
+#ifndef FISH_HANDLER_MNEMONICS_HPP_
+#define FISH_HANDLER_MNEMONICS_HPP_
+
+#include <stdint.h>
+
+enum fish_handler_mnemonics : uint16_t
+{
+	FISH_MNEMONIC_UNKNOWN = 0x8000,
+	
+	FISH_MNEMONIC_ALIGN,
+};
+
+#endif
