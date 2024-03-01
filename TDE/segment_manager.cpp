fdefe0183023a105429e09786e17c8cd7116dfa1
Soda <124202915+sodareverse@users.noreply.github.com>
2024-03-01 16:31:51 +0000

    Remove unnecessary prefixes
    Class and function names are in CamelCase for consistency and readability.
    Improved indentation and spacing for better readability.
    Renamed variables to follow camelCase convention.

--- TDE/segment_manager.cpp
+++ TDE/segment_manager.cpp
@@ -1,70 +1,50 @@
 #include "segment_manager.hpp"
-
 #include <idp.hpp>
 #include <segment.hpp>
 
-bool segment_manager::make_segment_copy(uint32_t address, std::size_t index)
-{
-	for (int32_t seg_index = 0, seg_count = get_segm_qty(); seg_index < seg_count; seg_index++)
-	{
-		segment_t* segment = getnseg(seg_index);
-
-		if (address >= segment->startEA && address < segment->endEA)
-		{
-			if (index < this->vm_segments.size())
-				this->vm_segments.erase(this->vm_segments.begin() + index);
-
-			this->vm_segments.insert(this->vm_segments.begin() + index, segment_copy(segment->startEA, segment->endEA));
-			return true;
-		}
-	}
-
-	return false;
+bool SegmentManager::makeSegmentCopy(uint32_t address, std::size_t index) {
+    for (int32_t segIndex = 0, segCount = get_segm_qty(); segIndex < segCount; ++segIndex) {
+        segment_t* segment = getnseg(segIndex);
+        if (address >= segment->startEA && address < segment->endEA) {
+            if (index < vmSegments.size())
+                vmSegments.erase(vmSegments.begin() + index);
+            vmSegments.insert(vmSegments.begin() + index, SegmentCopy(segment->startEA, segment->endEA));
+            return true;
+        }
+    }
+    return false;
 }
 
-bool segment_manager::is_segment(uint32_t address, std::size_t index)
-{
-	if (index >= this->vm_segments.size())
-		return false;
-
-	uint32_t start = this->vm_segments.at(index).start();
-	uint32_t end = this->vm_segments.at(index).end();
-
-	return (address >= start && address < end);
+bool SegmentManager::isSegment(uint32_t address, std::size_t index) {
+    if (index >= vmSegments.size())
+        return false;
+    uint32_t start = vmSegments.at(index).start();
+    uint32_t end = vmSegments.at(index).end();
+    return (address >= start && address < end);
 }
 
-bool segment_manager::in_segment_range(uint32_t address, uint32_t offset, std::size_t index)
-{
-	if (index >= this->vm_segments.size())
-		return false;
-	
-	uint32_t start = this->vm_segments.at(index).start();
-	uint32_t size = this->vm_segments.at(index).size();
-
-	return (((address - start) + offset) < size);
+bool SegmentManager::inSegmentRange(uint32_t address, uint32_t offset, std::size_t index) {
+    if (index >= vmSegments.size())
+        return false;
+    uint32_t start = vmSegments.at(index).start();
+    uint32_t size = vmSegments.at(index).size();
+    return ((address - start) + offset) < size;
 }
 
-std::pair<uint8_t*, uint32_t> segment_manager::to_segment(uint32_t address, std::size_t index)
-{
-	return std::make_pair(this->to_segment_base(address, index), this->to_segment_size(address, index));
+std::pair<uint8_t*, uint32_t> SegmentManager::toSegment(uint32_t address, std::size_t index) {
+    return std::make_pair(toSegmentBase(address, index), toSegmentSize(address, index));
 }
 
-uint8_t* segment_manager::to_segment_base(uint32_t address, std::size_t index)
-{
-	if (!this->is_segment(address, index))
-		return nullptr;
-	
-	uint32_t start = this->vm_segments.at(index).start();
-	
-	return this->vm_segments.at(index).data(address - start);
+uint8_t* SegmentManager::toSegmentBase(uint32_t address, std::size_t index) {
+    if (!isSegment(address, index))
+        return nullptr;
+    uint32_t start = vmSegments.at(index).start();
+    return vmSegments.at(index).data(address - start);
 }
 
-uint32_t segment_manager::to_segment_size(uint32_t address, std::size_t index)
-{
-	if (!this->is_segment(address, index))
-		return 0;
-	
-	uint32_t start = this->vm_segments.at(index).start();
-	
-	return (this->vm_segments.at(index).size() - (address - start));
+uint32_t SegmentManager::toSegmentSize(uint32_t address, std::size_t index) {
+    if (!isSegment(address, index))
+        return 0;
+    uint32_t start = vmSegments.at(index).start();
+    return (vmSegments.at(index).size() - (address - start));
 }
