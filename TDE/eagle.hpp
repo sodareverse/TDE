604b3ce3456c91f850a9986f9b6035e6e754a5c9
Soda <124202915+sodareverse@users.noreply.github.com>
2024-03-01 16:07:20 +0000

    Create eagle.hpp

---
+++ TDE/eagle.hpp
@@ -0,0 +1,20 @@
+#ifndef EAGLE_HPP_
+#define EAGLE_HPP_
+
+#include "wild_hybrid_base.hpp"
+
+#include "fish.hpp"
+#include "dolphin.hpp"
+
+class eagle : public wild_hybrid_base<fish, dolphin>
+{
+public:
+	bool is_signature(instruction_container& vm_entrance);
+	bool devirtualize(uint32_t vm_function, uint32_t vm_entry, instruction_container& vm_instructions);
+};
+
+/*
+
+*/
+
+#endif
