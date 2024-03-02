b02f76c9360d8516a2184a33e71b7ca631259a67
Soda <124202915+sodareverse@users.noreply.github.com>
2024-03-02 12:49:03 +0000

    Changes to the wild_opcode_label_manager class
    Changed the reset_labels function to use this->virtual_opcode_labels.clear() for clearing the vector. Using reserve(0x1000) after clear() is redundant and unnecessary since clear() resets the capacity as well.
    Renamed find_label_unread to find_unread_label to maintain consistency with other function names.
    Renamed find_label_by_address to find_label_at_address for clarity and consistency with the action performed.

--- TDE/wild_opcode_label_manager.hpp
+++ TDE/wild_opcode_label_manager.hpp
@@ -10,48 +10,47 @@
 class wild_opcode_label_manager
 {
 public:
-	void reset_labels()
-	{
-		this->virtual_opcode_labels.clear();
-		this->virtual_opcode_labels.reserve(0x1000);
-	}
+    void reset_labels()
+    {
+        this->virtual_opcode_labels.clear();
+    }
 
 public:
-	bool find_label_unread(std::vector<wild_opcode_label>::iterator& iter)
-	{
-		iter = std::find_if(this->virtual_opcode_labels.begin(), this->virtual_opcode_labels.end(), [&](wild_opcode_label const& label) -> bool
-		{
-			return (label.is_read == false);
-		});
-	
-		return (iter != this->virtual_opcode_labels.end());
-	}
+    bool find_unread_label(std::vector<wild_opcode_label>::iterator& iter)
+    {
+        iter = std::find_if(this->virtual_opcode_labels.begin(), this->virtual_opcode_labels.end(), [&](wild_opcode_label const& label) -> bool
+        {
+            return (!label.is_read);
+        });
+    
+        return (iter != this->virtual_opcode_labels.end());
+    }
 
-	bool find_label_by_address(std::vector<wild_opcode_label>::iterator& iter, uint32_t address)
-	{
-		iter = std::find_if(this->virtual_opcode_labels.begin(), this->virtual_opcode_labels.end(), [&](wild_opcode_label const& label) -> bool
-		{
-			return (label.address == address);
-		});
-	
-		return (iter != this->virtual_opcode_labels.end());
-	}
+    bool find_label_at_address(std::vector<wild_opcode_label>::iterator& iter, uint32_t address)
+    {
+        iter = std::find_if(this->virtual_opcode_labels.begin(), this->virtual_opcode_labels.end(), [&](wild_opcode_label const& label) -> bool
+        {
+            return (label.address == address);
+        });
+    
+        return (iter != this->virtual_opcode_labels.end());
+    }
 
 public:
-	bool exists_label(uint32_t address)
-	{
-		std::vector<wild_opcode_label>::iterator iter;
-		return this->find_label_by_address(iter, address);
-	}
+    bool exists_label(uint32_t address)
+    {
+        std::vector<wild_opcode_label>::iterator iter;
+        return this->find_label_at_address(iter, address);
+    }
 
-	void create_label(uint32_t address, uint32_t offset)
-	{
-		if (!this->exists_label(address))
-			this->virtual_opcode_labels.push_back(wild_opcode_label(address, offset));
-	}
+    void create_label(uint32_t address, uint32_t offset)
+    {
+        if (!this->exists_label(address))
+            this->virtual_opcode_labels.push_back(wild_opcode_label(address, offset));
+    }
 
 protected:
-	std::vector<wild_opcode_label> virtual_opcode_labels;
+    std::vector<wild_opcode_label> virtual_opcode_labels;
 };
 
 #endif
